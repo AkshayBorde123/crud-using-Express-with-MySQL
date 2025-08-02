e491ed4602e0702434b7310b043419c72ee02a48
AkshayBorde123 <bordeakshay2004@gmail.com>
2025-08-02 20:15:56 +0530

    modified index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true A
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`app is runnig on ${port}`);
